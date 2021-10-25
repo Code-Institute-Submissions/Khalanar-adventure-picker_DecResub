c83691b71c7af2bea4c0365848e005caac77c500
Khalanar <pablo.xez@gmail.com>
2021-10-25 16:28:59 +0100

    now a separate function can be passed as an event

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
 class Event():
     def __init__(self, text, next_event_0, next_event_1):
         self.text = text
@@ -8,28 +6,45 @@ class Event():
         self.next_event_1 = next_event_1
     
     def run(self):
-        print(f">>{self.text}")
-        user_input = input(f"Do you -{self.next_event_0[0]}- or -{self.next_event_1[0]}-?\n")
+        print(f"\n>>{self.text}")
+        user_input = input(f"Do you -{self.next_event_0[0]}- or -{self.next_event_1[0]}-?\nYou ")
+        
         if (user_input == self.next_event_0[0]):
-            self.next_event_0[1].run()
+            if (callable(self.next_event_0[1])):
+                print("its a function")
+                self.next_event_0[1]
+            else:
+                print("its NOT a function")
+                self.next_event_0[1].run()
+       
         elif (user_input == self.next_event_1[0]):
-            self.next_event_1[1].run()
+            if (callable(self.next_event_1[1])):
+                self.next_event_1[1]
+            else:
+                self.next_event_1[1].run()
         else:
-            print("Invalid option, should loop back to the start")
+            print(f"\n-{user_input}- is not an option")
+            self.run()
         
 
-event_01 = Event("You said hello back",
-    ["event_01 Option1", ""],
-    ["event_01 Option2",""])
+def do_something():
+    print("something was done")
 
-event_00 = Event("You said hello back",
-    ["event_00 Option1", ""],
-    ["event_00 Option2",""])
+event_01 = Event("Bold option my friend, the wizard is not amused",
+    ["Option1", ""],
+    ["Option2", ""])
+
+event_00 = Event("Good good, you're one step closer to learning the truth",
+    ["Option1", ""],
+    ["Option2", ""])
     
 game_start = Event("Game Start, what will you do?",
-    ["say hello back", event_00],
+    ["say hello", do_something],
     ["say bye", event_01])
 
 
 
-game_start.run()
+def main():
+    game_start.run()
+
+main()
