8dd1bdef79816b972ec6845711175f4cdda4a453
Khalanar <pablo.xez@gmail.com>
2021-11-09 10:54:17 +0000

    fix spelling errors

--- events.py
+++ events.py
@@ -19,7 +19,7 @@ boar_combat = Combat(boar, player)
 # matter when they have their values assigned
 game_start = Event()  #
 check_wallet = Event()  #
-unsheath_sword = Event()  #
+unsheathe_sword = Event()  #
 approach_bush = Event()  #
 dead_rat = Event()  #
 run_from_bushes = Event()  #
@@ -46,25 +46,25 @@ restart_game = Event()
 game_start.create_event(
     "You wake up in the middle of a forest. You are not sure where you are. Judging by the amount of moss in the trees around you, somewhere in the middle of Spring in the northern hemisphere. You have no memories whatsoever of ever having come to this forest, and something about it unsettles your stomach\nYour trusty sword is firmly sheathed, which is a good sign, you have not been robbed. Or so you think.",
     [["Check coin purse", check_wallet],
-     ["Unsheath sword", unsheath_sword]])
+     ["unsheathe sword", unsheathe_sword]])
 
 check_wallet.create_event(
     f"You reach out to your coin purse but it's not in your belt. At least nowhere to be found. You start to panic but realize you're wearing it to the right of your belt instead of the left. Odd, you have always worn it in your left side. There's {player.gold} coins in it. You don't remember how much you had left, or when was the last time you actually used it.",
-    [["Unsheath sword", unsheath_sword],
+    [["unsheathe sword", unsheathe_sword],
      ["Stand up", stand_up]])
 
-unsheath_sword.create_event(
-    "You reach out to your sword but can't unsheathe it. It feels stuck. Upon closer inspection you realise it's not all the way in. You pull harder and manage to unsheath your steel, completely covered in dried-out blood so thick that you think it might have been responsible for ruining your prime-leather scabbard. While you lament the moment you ever put your sword away before cleaning it, the bushes to your left start rustling.",
+unsheathe_sword.create_event(
+    "You reach out to your sword but can't unsheathe it. It feels stuck. Upon closer inspection you realise it's not all the way in. You pull harder and manage to unsheathe your steel, completely covered in dried-out blood so thick that you think it might have been responsible for ruining your prime-leather scabbard. While you lament the moment you ever put your sword away before cleaning it, the bushes to your left start rustling.",
     [["Check bushes", approach_bush],
      ["run", run_from_bushes]])
 
 approach_bush.create_event(
-    "Your brain tells you your sword will be practically useless in this state but your heart is pumping with anticipation. As you approach the bushes, a rat comes out. It's not a normal rat. There is something vicious about the way it looks at you. Like an animal without any self-preservation insticts, like an animal taken away by something dark that you can't begin to comprehend",
+    "Your brain tells you your sword will be practically useless in this state but your heart is pumping with anticipation. As you approach the bushes, a rat comes out. It's not a normal rat. There is something vicious about the way it looks at you. Like an animal without any self-preservation instincts, like an animal taken away by something dark that you can't begin to comprehend.",
     [["Attack", dead_rat, rat_combat],
      ["run", run_from_bushes]])
 
 dead_rat.create_event(
-    "The rat lies dead. Its severed body now covered in blood and soil emits a hissing sound while thick blood comes out from the hole where the head used to be. You feel a strange urge to carry the animal's remains but at the same time it does not seem like a good idea. You are disgusted and at the same time atracted to the creature's oozing blood.",
+    "The rat lies dead. Its severed body now covered in blood and soil emits a hissing sound while thick blood comes out from the hole where the head used to be. You feel a strange urge to carry the animal's remains but at the same time it does not seem like a good idea. You are disgusted and at the same time attracted to the creature's oozing blood.",
     [["inspect corpse", inspect_corpse],
      ["carry corpse", carry_corpse]])
 
@@ -79,12 +79,12 @@ wounded_boar.create_event(
      ["Lay hands on boar", lay_hands]])
 
 heal_boar.create_event(
-    "You sense a deep connection to this animal and regret the minute you lay hands on her. 'Her?' you say. You never checked yet you know this animal is a female. You apply some ointment in the animal's wounds and feed her some dried fruits you were carrying. While caressing her mane you speak words of forgiveness and stay with her in her last moments. A few minutes later, the animal stands back up and presses her body against yours right before, as a well trained dog, sits on her two back legs. You immediately understand what is going on.\n\n\nThis path will eventually lead you to becoming a Speaker of Anuk.\n\nChapter 2 coming soon:",
+    "You sense a deep connection to this animal and regret the minute you lay hands on her. 'Her?' you say. You never checked yet you know this animal is a female. You apply some ointment in the animal's wounds and feed her some dried fruits you were carrying. While caressing her mane you speak words of forgiveness and stay with her in her last moments. A few minutes later, the animal stands back up and presses her body against yours, right before, like a well-trained dog, sits on her two back legs. You immediately understand what is going on.\n\n\nThis path will eventually lead you to becoming a Forest Guardian.\n\nChapter 2 coming soon:",
     [["Restart", game_start],
     ["Exit", sys.exit]])
 
 lay_hands.create_event(
-    "Posessed by something you can't yet comprehend, you lay your hands on the animal's head and open your mouth. You feel your head starts vibrating in a low frequency that starts to pick up as it echoes in the boar's body through your hands. With your mouth still wide open some words come from it. You never pronouned those words, you don't know those words and they don't sound like any language you have ever heard before. The now high-pitched rining suddenly stops and the boar gets up and starts running to the woods as if it was completely overwhelmed by what just happened.\n\n\nThis path will eventually lead you to becoming a Forest Guardian.\n\nChapter 2 coming soon:",
+    "Posessed by something you can't yet comprehend, you lay your hands on the animal's head and open your mouth. You feel your head starts vibrating in a low frequency that starts to pick up as it echoes in the boar's body through your hands. With your mouth still wide open some words come from it. You never pronouned those words, you don't know those words and they don't sound like any language you have ever heard before. The now high-pitched rining suddenly stops and the boar gets up and starts running to the woods as if it was completely overwhelmed by what just happened.\n\n\nThis path will eventually lead you to becoming a Speaker of Anuk.\n\nChapter 2 coming soon:",
     [["Restart", game_start],
     ["Exit", sys.exit]])
 
@@ -96,7 +96,7 @@ reach_town.create_event(
 stand_up.create_event(
     "You stand up, you feel dizzy. You're unsure if for the lack of food or lack of sleep, but it's certainly a feeling you are not used to. Before you can even make sense to whatever is going on with you, your gut rumbles, there is something odd about the bushes next to you.",
     [["Approach",approach_bushes_gameover],
-    ["Unsheath sword", unsheath_sword]])
+    ["unsheathe sword", unsheathe_sword]])
 
 inspect_corpse.create_event(
     "You get closer to the rat's dead body. The rate at which the blood comes out of is down to a trickle and its slightly sweet arome fills the air. It's a strangely comforting smell however repulsive the image before you may be. You can't take this anymore, clean your sword against your pantalons and continue down a trail near you. Eventually you can the running waters of a stream. Judging by the pitch of the water moving through the rocks you can tell it's cold. Colder than what the biome in the area may suggest. This may be ice water trickling down from a nearby mountain. If there's snowpure water, there must be a town somewhere along the stream so you decide to follow it.\n\nAfter some hours down-stream, you can hear chanting. Beautiful chanting. After getting closer you realise these aren't human voices, it's lamias. These half-fish creatures are known to roam from town to town, stealing kids off their homes and devouring them. These are not figments of an overly cautious society telling tales to avoid their little ones to stray too far away. Lamias are real, and four of them are right in front of you, unsuspecting.",
@@ -104,7 +104,7 @@ inspect_corpse.create_event(
      ["Attack Lamias", attack_lamias]])
 
 carry_corpse.create_event(
-    "The thought of carrying this dead rat repulses you but at the same time you feel it's the right thing to do. You have to fight against your insticts to be able to even touch it. You grab the rat and shove it into your coin purse. You gag as you pull your hand from the purse. Why are you even doing this? It's almost as there is a second voice in your head pulling you into doing this. You are so disgusted by all this that thread closed the coin sack and continue down the small pathway.\n\nIt's getting dark and cold. The still warm cadaver is the only source of heat you have right now. As you think about making camp, you see a strange greenish light. You don't want to get closer but you haven't eaten in a day and the smell of cinnamon rolls draws you to the source.\n\nYou see an old man in dark robes with silver threading. As you accept the old man's request he starts talking to you about a source of inconmensurable power. By the way he speaks you can't avoid but thinking that all your recent actions have been in some way influenced by this man and the power he speaks of. Can this even be? You think you're going crazy.\n\nThe old man suggests you join him, in his quest for power in his quest for power there's space for a young apprentice.",
+    "The thought of carrying this dead rat repulses you but at the same time you feel it's the right thing to do. You have to fight against your insticts to be able to even touch it. You grab the rat and shove it into your coin purse. You gag as you pull your hand from the purse. Why are you even doing this? It's almost as there is a second voice in your head pulling you into doing this. You are so disgusted by all this that thread closed the coin sack and continue down the small pathway.\n\nIt's getting dark and cold. The still warm cadaver is the only source of heat you have right now. As you think about making camp, you see a strange greenish light. You don't want to get closer but you haven't eaten in a day and the smell of cinnamon rolls draws you to the source.\n\nYou see an old man in dark robes with silver threading. As you accept the old man's request he starts talking to you about a source of immeasurable power. By the way he speaks you can't avoid but thinking that all your recent actions have been in some way influenced by this man and the power he speaks of. Can this even be? You think you're going crazy.\n\nThe old man suggests you join him, in his quest for power in his quest for power there's space for a young apprentice.",
     [["Accept", agree_necro],
      ["Attack", disagree_necro, necromancer_combat]])
 
@@ -124,12 +124,12 @@ approach_lamias.create_event(
     ["Exit", sys.exit]])
 
 attack_lamias.create_event(
-    "As you unsheath your sword, the four demons stop singing. You bland your sword with such swiftness that the air around it whistles excitedly. You slash so quickly through one of the demons neck, that before the beheaded body touches the ground your blade is already handle deep into another one's chest stopping her heart immediately. The third Lamia opens her mouth in an act you interpret as singing. If she starts singing you may be drawn into doing things you don't want to. These is how these demons mind-control their victims. Before she can mutter a word, you slide your sword into her mouth. With her eyes fixed to you, you start to feel shame for her as she starts gargling blood. Not 'her', 'it' you think to yourself as you pull the sword back from her and pull the corpse to a side in the same motion.\n\nThe fourth Lamia drops to her knees with her hands to the ground and speaks 'we are sorry'. This fills you with an uncontrollable rage as you remember the smell of fish and the  the trail one of these disgusting creatures next to your baby sister's crib some odd 10 years ago. You raise your sword with both hands over your head and a scream of rage accompanies your slash towards the creature's head. Right before the blade strikes the Lamia speaks again, 'thank you'.\n\n\nThis path will eventually lead you to becoming a Demon Slayer.\n\nChapter 2 coming soon:",
+    "As you unsheathe your sword the four demons stop singing. You bland your sword with such swiftness that the air around it whistles excitedly. You slash so quickly through one of the demons neck, that before the beheaded body touches the ground your blade is already handle deep into another one's chest stopping her heart immediately. The third Lamia opens her mouth in an act you interpret as singing. If she starts singing you may be drawn into doing things you don't want to. These is how these demons mind-control their victims. Before she can mutter a word, you slide your sword into her mouth. With her eyes fixed to you, you start to feel shame for her as she starts gargling blood. Not 'her', 'it' you think to yourself as you pull the sword back from her and pull the corpse to a side in the same motion.\n\nThe fourth Lamia drops to her knees with her hands to the ground and speaks 'we are sorry'. This fills you with an uncontrollable rage as you remember the smell of fish and the  the trail one of these disgusting creatures next to your baby sister's crib some odd 10 years ago. You raise your sword with both hands over your head and a scream of rage accompanies your slash towards the creature's head. Right before the blade strikes the Lamia speaks again, 'thank you'.\n\n\nThis path will eventually lead you to becoming a Demon Slayer.\n\nChapter 2 coming soon:",
     [["Restart", game_start],
     ["Exit", sys.exit]])
 
 help_execution.create_event(
-    "Public executions are not a joke. Maintaining law and order are not a joke. Enforcing the laws of men as handed down by the Gods is not a joke. 'ENOUGH' you scream as you make your way towards the gallows through the cheerful mass. You unsheath the sword as you climb the stairs and without even looking at the prisoner you slice his whole arm off and pierce through the executioners heart in the same strike.\n\nThe crowd goes silent and looks at you while you wipe off the blood on your sword in your forearm.\n\n\nThis path will eventually lead you to becoming the Emperor's Sword.\n\nChapter 2 coming soon:",
+    "Public executions are not a joke. Maintaining law and order is not a joke. Enforcing the laws of men as handed down by the Gods is not a joke. 'ENOUGH' you scream as you make your way towards the gallows through the cheerful mass. You unsheathe the sword as you climb the stairs and without even looking at the prisoner you slice his whole arm off and pierce through the executioners heart in the same strike.\n\nThe crowd goes silent and looks at you while you wipe off the blood on your sword in your forearm.\n\n\nThis path will eventually lead you to becoming the Emperor's Sword.\n\nChapter 2 coming soon:",
     [["Restart", game_start],
     ["Exit", sys.exit]])
 
